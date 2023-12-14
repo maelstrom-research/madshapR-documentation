2212c904e76a958fcc7d3b63c46194f5dd046b49
guifabre <guijoseph.fabre@gmail.com>
2023-12-14 13:46:36 -0500

    'update documentation following latest version of the package'

--- update_files.R
+++ update_files.R
@@ -15,6 +15,39 @@ fs::dir_copy(
   overwrite = TRUE)
 
 # switch to madshapR-documentation
+
+file.edit("docs/authors.html")
+
+# apres la ligne 70
+
+<div class="section level2 aknowledgment-section">
+  <div>
+  <h1 id="citation">Acknowledgment</h1>
+  
+  
+  We extend our heartfelt appreciation to all the members of the Maelstrom Research 
+  team and our valued partners who have played an integral role in making this project 
+  possible. Special thanks go to:
+  
+  <ul><br>
+  
+  <li><strong>Alexandre Trottier</strong> and <strong>Tina Wey</strong> for 
+      their contributions to the documentation of the package.</li>
+  <li><strong>Rita Wissa</strong> and <strong>Anouar Nechba</strong> for their  
+      input in aligning the package with scientific guidelines and Maelstrom standards.</li>
+  <li><strong>Samuel El-Bouzaidi Tiali</strong> and <strong>Zishu Chen</strong> and 
+      all of your github beta-testers for their dedicated efforts in testing the package 
+      and providing insightful enhancements.</li>
+  
+  </ul>
+  
+  Thank you for your outstanding contributions.
+
+</div>
+  </div>
+
+
+
 # push to git
 
 'update documentation following latest version of the package'
