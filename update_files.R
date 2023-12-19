a0c8cf5128b81b3055b1d0523b3e17514ad48b20
guifabre <guijoseph.fabre@gmail.com>
2023-12-19 13:46:35 -0500

    Update documentation following latest version of the package

--- update_files.R
+++ update_files.R
@@ -8,6 +8,40 @@ devtools::build_rmd('NEWS.Rmd')
 fs::dir_delete("docs")
 pkgdown::build_site()
 
+file.edit("docs/authors.html")
+# apres la ligne 69
+
+   <div class="section level2 aknowledgment-section">
+     <div>
+     <h1 id="aknowledgment">Acknowledgment</h1>
+
+
+We extend our heartfelt appreciation to all the members of the
+Maelstrom Research team and our valued partners who have played a role in
+making this project possible.
+
+<br><br>
+Special thanks go to
+
+<strong>Zishu Chen</strong>,
+<strong>Samuel El-Bouzaidi Tiali</strong>,
+<strong>Anouar Nechba</strong>,
+<strong>Alexandre Trottier</strong>,
+<strong>Tina Wey</strong>,
+<strong>Rita Wissa</strong> and
+all of your github beta-testers for their contributions to the documentation
+of the package, input in aligning the package with scientific guidelines and
+Maelstrom standards and for their dedicated efforts in testing the package and
+providing insightful enhancements.
+
+<br><br>
+
+Thank you for your outstanding contributions.
+<br><br>
+
+     </div>
+   </div>
+
 fs::dir_delete("../madshapR-documentation/docs")
 fs::dir_copy(
   "docs",
@@ -15,38 +49,6 @@ fs::dir_copy(
   overwrite = TRUE)
 
 # switch to madshapR-documentation
-
-file.edit("docs/authors.html")
-
-# apres la ligne 70
-
-<div class="section level2 aknowledgment-section">
-  <div>
-  <h1 id="citation">Acknowledgment</h1>
-  
-  
-  We extend our heartfelt appreciation to all the members of the Maelstrom Research 
-  team and our valued partners who have played an integral role in making this project 
-  possible. Special thanks go to:
-  
-  <ul><br>
-  
-  <li><strong>Alexandre Trottier</strong> and <strong>Tina Wey</strong> for 
-      their contributions to the documentation of the package.</li>
-  <li><strong>Rita Wissa</strong> and <strong>Anouar Nechba</strong> for their  
-      input in aligning the package with scientific guidelines and Maelstrom standards.</li>
-  <li><strong>Samuel El-Bouzaidi Tiali</strong> and <strong>Zishu Chen</strong> and 
-      all of your github beta-testers for their dedicated efforts in testing the package 
-      and providing insightful enhancements.</li>
-  
-  </ul>
-  
-  Thank you for your outstanding contributions.
-
-</div>
-  </div>
-
-
 
 # push to git
 
